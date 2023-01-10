f78264a01004855bc13f16c5ca09e1bc985a6e20
jacob <jacobgidron@mail.tau.ac.il>
2023-01-10 14:22:46 +0200

    cheand version

--- amq_cheat.py
+++ amq_cheat.py
@@ -36,44 +36,21 @@ driver.get(url)
 # context = driver.find_element('xpath',
 #       r"/html/body/div[2]/div[1]/div[7]/div[2]/div[2]/div[1]/div/div[1]/textarea").text
 # context = driver.find_element('xpath',"/html/body/div[2]/div[1]/div[7]/div[1]/div[2]/div[8]/div[2]/div[3]/div[2]/input")
-async def recognize(context=None):
+async def recognize():
     file_path = r" Z:\TEMP\sample.mp3"
     cmd = """./ffmpeg.exe -f dshow -i audio="VoiceMeeter Output (VB-Audio VoiceMeeter VAIO)" -y -t 00:00:04 Z:/TEMP/sample.mp3"""
     # cmd = """ ./ffmpeg.exe -f dshow -i audio="VoiceMeeter Output (VB-Audio VoiceMeeter VAIO)" -y -t 00:00:04 sample.mp3"""
     print(cmd)
     args = shlex.split(cmd)
-
     subprocess.Popen(args)
     sleep(6)
     shazam = Shazam()
     out = await shazam.recognize_song(r'Z:\TEMP\sample.mp3')
-    if ('track' not in out) or 'title' not in out['track']:
-        return None, None, False
+    if ('track' not in out.keys()) or 'title' not in out['track']:
+        return False, None
     songe_title = out['track']['title']
-    songe_title_clean = re.sub(r'\(.*\)', '', songe_title).lower()
     print(songe_title)
-    print(songe_title_clean)
-    tmp = requests.get(f"https://api.animethemes.moe/search?q={{{songe_title_clean}}}&page[limit]=1").json()
-    if not tmp['search']['videos']:
-        return songe_title, None, False
-    tmp_name = tmp['search']['videos'][0]['basename']
-    print(tmp_name)
-    camel_name = re.findall("[^-]+", tmp_name)[0]
-    print(camel_name)
-    lower_name = re.sub(r'(?<!^)(?=[A-Z])', ' ', camel_name).lower()
-    name_search = requests.get(f"https://api.animethemes.moe/search?q={{{lower_name}}}&page[limit]=1").json()
-    if not tmp['search']['anime']:
-        return songe_title, None, False
-    name = name_search['search']['anime'][0]['name']
-    print(f"entering res:{name}")
-    if context is None:
-        context = driver.find_element('xpath',
-                                      "/html/body/div[2]/div[1]/div[7]/div[1]/div[2]/div[8]/div[2]/div[3]/div[2]/input")
-    context.send_keys(f"{name}\n")
-
-    # https://api.animethemes.moe/search?q={Bocchi%20The%20Rock}&page[limit]=1
-
-    return songe_title, name, True
+    return True, songe_title
 
 
 loop = asyncio.get_event_loop()
@@ -91,14 +68,30 @@ loop = asyncio.get_event_loop()
 
 # start button
 # /html/body/div[2]/div[1]/div[7]/div[1]/div[1]/div[1]/div[4]/h1
+def find_anime(song):
+    songe_title_clean = re.sub(r'\(.*\)', '', song).lower()
+    print(songe_title_clean)
+    tmp = requests.get(f"https://api.animethemes.moe/search?q={{{songe_title_clean}}}&page[limit]=1").json()
+    if not tmp['search']['videos']:
+        return None
+    tmp_name = tmp['search']['videos'][0]['basename']
+    print(tmp_name)
+    camel_name = re.findall("[^-]+", tmp_name)[0]
+    print(camel_name)
+    lower_name = re.sub(r'(?<!^)(?=[A-Z])', ' ', camel_name).lower()
+    name_search = requests.get(f"https://api.animethemes.moe/search?q={{{lower_name}}}&page[limit]=1").json()
+    if not tmp['search']['anime']:
+        return None
+    name = name_search['search']['anime'][0]['name']
+    return name
 
 def play_game():
     tmp = pd.read_json('mem.jsonl', lines=True)
-
     context = driver.find_element('xpath',
                                   "/html/body/div[2]/div[1]/div[7]/div[1]/div[2]/div[8]/div[2]/div[3]/div[2]/input")
     j = tmp.to_dict(orient='list')
     mem_dict = dict(zip(j['song'], j['anime']))
+
     ref_idx = driver.find_element('xpath',
                                   "/html/body/div[2]/div[1]/div[7]/div[1]/div[2]/div[8]/div[2]/div[1]/div[1]/div/span[1]")
     true_ans = driver.find_element('xpath',
@@ -106,43 +99,41 @@ def play_game():
     count = driver.find_element('xpath',
                                 "/html/body/div[2]/div[1]/div[7]/div[1]/div[2]/div[8]/div[2]/div[2]/div/div/div[1]/div[5]/div")
     while true_ans.is_displayed():
-        # in_loop = asyncio.get_event_loop()
-        # song, anime, status = await recognize()
-        song, anime, status = loop.run_until_complete(recognize(context))
-        # song, anime, status = in_loop.run_until_complete(recognize())
-        # we failed
+        status = False
+        song = loop.run_until_complete(recognize())
+        left = count.text
+        while not status:
+            status, song = loop.run_until_complete(recognize())
+
+        if (song is not None):
+            if (song in mem_dict.keys()):
+                print(f"remembered that {song} is form  {mem_dict.get(song)} ")
+                anime = mem_dict.get(song)
+            else:
+                anime = find_anime(song)
+            try:
+                context.send_keys(anime)
+            except ElementNotInteractableException:
+                print("ElementNotInteractableException giving up")
+        else:
+            print("song is None, giving up")
 
         # wait for song revel and save answer
-        left = count.text
-
         while left != '':
-            if (song is not None) and (song in mem_dict.keys()) and not status:
-                print(f"rememberd that {song} is form  {mem_dict.get(song)} ")
-                try:
-                    context.send_keys(mem_dict.get(song))
-                    status = True
-                except ElementNotInteractableException:
-                    print("ElementNotInteractableException giving up")
-
-            elif song is None:
-                try:
-                    song, anime, status = loop.run_until_complete(recognize())
-                except ElementNotInteractableException:
-                    print("ElementNotInteractableException in recognize giving up")
-                    break
             print(f"{left} seconds left")
             sleep(2)
             left = count.text
-        last_ans = true_ans.text
+
         if (song is not None) and (song not in mem_dict.keys()):
-            print(f"savimg {song}  with \b {last_ans}")
-            mem_dict[song] = last_ans
+            ans = true_ans.text
+            print(f"savimg {song}  with \b {ans}")
+            mem_dict[song] = ans
             with open("mem.jsonl", "a") as mem_file:
                 mem_file.write(json.dumps({"song": song,
-                                           "anime": last_ans}))
+                                           "anime": ans}))
                 mem_file.write("\n")
         # wait to next round
-        print("wating for next round")
+        print("waiting for next round")
         song_idx = ref_idx.text
         print(f"{song_idx} == {ref_idx.text}")
         while song_idx == ref_idx.text:
